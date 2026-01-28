58040530360ee8c38288ea3cc0dfc1576dbf1581
KUba935 <kubsonix198@gmail.com>
2026-01-28 15:30:07 +0100

    Add files via upload

--- joiny.sql
+++ joiny.sql
@@ -1,3 +1,4 @@
+Podstawowe
 SELECT AVG(Cena) as sr_cena FROM ksiazki
 SELECT COUNT(id_ksiazki) AS ilosc_ksiazek FROM ksiazki;
 SELECT tytul, max(Cena) AS najtansza_ksiazka FROM ksiazki;
@@ -9,7 +10,7 @@ SELECT lower(wydawnictwo) FROM wydawnictwa;
 SELECT length(tytul) FROM ksiazki;
 SELECT substring(imie, 1, 3) AS trzy_pierwsze_litery FROM klienci;
 SELECT UPPER(tytul) FROM ksiazki;
-
+Średnio zaawansowane
 SELECT round(AVG(Cena), 2) FROM ksiazki;
 SELECT max(Cena) - min(Cena) AS roznica_cen FROM ksiazki;
 SELECT * FROM ksiazki GROUP BY id_ksiazki HAVING AVG(ksiazki.Cena) < ksiazki.Cena;
@@ -21,5 +22,5 @@ SELECT REPLACE(tytul, " ", "_") FROM ksiazki;
 SELECT tytul FROM ksiazki WHERE tytul LIKE '%a%';
 SELECT a.tytul, b.gatunek FROM ksiazki a INNER JOIN gatunki b on a.id_gatunku = b.id_gatunku
 SELECT substring(kod_pocztowy, 1, 2) FROM klienci;
-
+Zaawansowane
 SELECT round((Cena * SUM(Cena)) / 100, 2) AS udzial_w_proc FROM ksiazki GROUP BY id_ksiazki;
