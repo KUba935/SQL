68f7e098530fdc8c5bda10d52d808c7b4067a2d0
KUba935 <kubsonix198@gmail.com>
2026-02-04 14:51:25 +0100

    Add files via upload

--- joiny.sql
+++ joiny.sql
@@ -22,3 +22,5 @@ SELECT a.tytul, b.gatunek FROM ksiazki a INNER JOIN gatunki b on a.id_gatunku = 
 SELECT substring(kod_pocztowy, 1, 2) FROM klienci;
 Zaawansowane
 SELECT round((Cena * SUM(Cena)) / 100, 2) AS udzial_w_proc FROM ksiazki GROUP BY id_ksiazki;
+SELECT concat(imie," ",nazwisko) AS "klient" FROM klienci k INNER JOIN sprzedaz s ON s.id_klienta = k.id_klienta GROUP BY k.id_klienta HAVING AVG(s.id_klienta) < COUNT(s.id_klienta);
+SELECT p.imie, p.wynagrodzenie, (p.wynagrodzenie / AVG(p.wynagrodzenie)) * 100 FROM pracownicy p INNER JOIN stanowiska s ON p.id_stanowiska = s.id_stanowiska GROUP BY p.id_stanowiska;
