079f5a64ee7eb566d0ad0537755fa0187f90a6f8
KUba935 <kubsonix198@gmail.com>
2026-06-03 15:37:39 +0200

    Add new queries and restructure SQL file

--- egzpop.sql
+++ egzpop.sql
@@ -18,5 +18,10 @@ Grupa C
 
 Zad 6
 SELECT k.id_klienta, k.imie, k.nazwisko, COUNT(w.id_wypozyczenia) AS liczba_wypozyczen FROM klienci k LEFT JOIN wypozyczenia w ON k.id_klienta = w.id_klienta GROUP BY k.id_klienta, k.imie, k.nazwisko ORDER BY liczba_wypozyczen DESC;
-
-
+Zad 7
+SELECT s.marka, s.model, COUNT(w.id_wypozyczenia) AS liczba_wypozyczen FROM samochody s INNER JOIN wypozyczenia w ON s.id_samochodu = w.id_samochodu GROUP BY s.id_samochodu, s.marka, s.model ORDER BY liczba_wypozyczen DESC LIMIT 5;
+Zad 8
+SELECT k.imie, k.nazwisko, SUM(f.kwota_calosci) AS laczna_kwota FROM klienci k INNER JOIN faktury f ON k.id_klienta = f.id_faktury GROUP BY k.id_klienta, k.imie, k.nazwisko ORDER BY laczna_kwota DESC;
+Zad 9
+SELECT s.marka, s.model, COUNT(se.id_serwisu) AS liczba_serwisow FROM samochody s INNER JOIN serwisy se ON s.id_samochodu = se.id_samochodu GROUP BY s.id_samochodu, s.marka, s.model ORDER BY liczba_serwisow ASC;
+Zad 10
